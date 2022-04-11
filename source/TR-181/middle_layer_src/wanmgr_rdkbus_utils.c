70a3d73502a1b30fe64f3829b9310dbc36df9889
Andre McCurdy <armccurdy@gmail.com>
2022-04-11 00:01:36 -0700

    avoid strncat() with invalid buffer size limit
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/TR-181/middle_layer_src/wanmgr_rdkbus_utils.c
+++ source/TR-181/middle_layer_src/wanmgr_rdkbus_utils.c
@@ -230,7 +230,7 @@ ANSC_STATUS WaitForInterfaceComponentReady(char *pPhyPath)
 
 ANSC_STATUS WanMgr_RdkBus_SetRequestIfComponent(char *pPhyPath, char *pInputparamName, char *pInputParamValue, enum dataType_e type)
 {
-    char param_name[BUFLEN_256] = {0};
+    char param_name[BUFLEN_256];
     char  pCompName[BUFLEN_64] = {0};
     char  pCompPath[BUFLEN_64] = {0};
     char *faultParam = NULL;
@@ -244,8 +244,7 @@ ANSC_STATUS WanMgr_RdkBus_SetRequestIfComponent(char *pPhyPath, char *pInputpara
         return ANSC_STATUS_FAILURE;
     }
 
-    strncpy(param_name, pPhyPath, sizeof(param_name)-1);
-    strncat(param_name,pInputparamName,sizeof(param_name));
+    snprintf(param_name, sizeof(param_name), "%s%s", pPhyPath, pInputparamName);
 
     if(strstr(param_name, "CableModem") != NULL) { // CM wan interface
         strncpy(pCompName, CMAGENT_COMPONENT_NAME, sizeof(pCompName));
