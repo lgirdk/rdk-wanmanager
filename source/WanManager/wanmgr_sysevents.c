d00025851510989e34a3c08462a32024c910047d
Cibil <cpankiras.contractor@libertyglobal.com>
2022-05-02 11:49:21 +0200

    restart ntpd when dhcpv6_ntp_server sysevent is set ( OFW-2659 )
    ntpd daemon starts when sysevent "wan-status" becomes "started".
    But in dual-stack mode, this sysevent is set when erouter0 gets
    IPv4 address. Because of that, NTPv6 option from DHCPv6 is not
    applied to ntpd.
    
    As a fix, register a new sysevent dhcpv6_ntp_server in rdk-wanmanager
    and restart ntpd when "dhcpv6_ntp_server" is set.
    
    Signed-off-by: Cibil <cpankiras.contractor@libertyglobal.com>

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -456,6 +456,7 @@ static void *WanManagerSyseventHandler(void *args)
 #if defined (RDKB_EXTENDER_ENABLED)
     async_id_t mesh_wan_link_status_asyncid;
 #endif /* RDKB_EXTENDER_ENABLED */
+    async_id_t dhcpv6_ntp_server_asyncid;
 
     sysevent_set_options(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_ULA_ADDRESS, TUPLE_FLAG_EVENT);
     sysevent_setnotification(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_ULA_ADDRESS, &lan_ula_address_event_asyncid);
@@ -496,6 +497,9 @@ static void *WanManagerSyseventHandler(void *args)
     sysevent_set_options(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_MESH_WAN_LINK_STATUS, TUPLE_FLAG_EVENT);
     sysevent_setnotification(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_MESH_WAN_LINK_STATUS, &mesh_wan_link_status_asyncid);
 #endif /* RDKB_EXTENDER_ENABLED */
+
+    sysevent_set_options(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_DHCPV6_NTP_SERVER, TUPLE_FLAG_EVENT);
+    sysevent_setnotification(sysevent_msg_fd, sysevent_msg_token, SYSEVENT_DHCPV6_NTP_SERVER, &dhcpv6_ntp_server_asyncid);
 
     for(;;)
     {
@@ -738,6 +742,10 @@ static void *WanManagerSyseventHandler(void *args)
                 }
             }
 #endif
+            else if (strcmp(name, SYSEVENT_DHCPV6_NTP_SERVER) == 0)
+            {
+                sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_NTPD_RESTART, NULL, 0);
+            }
             else
             {
                 CcspTraceError(("%s %d undefined event %s:%s \n", __FUNCTION__, __LINE__, name, val));
