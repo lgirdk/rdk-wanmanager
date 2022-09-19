e850790ee96bda2c2309cb2501fa81169c9996c7
Oussema Ben Abdallah <obenabdallah@libertyglobal.com>
2022-09-19 16:57:02 +0200

    fix eRouter must not support SLAAC Method ( OFW-3369 )
    [Issue]
    SLAAC must not be supported.
    
    [Solution]
    Disable flag autoconf responsable for creating IPv6 GUA using SLAAC
    
    [Testing Performed]
    Check if the gateway is able to obtain IPv6 address using statefull
    mode.
    Check in case router adv flag A is set to true if the gateway is using
    SLAAC method to get IP or not. We should not use SLAAC on eRouter interface.
    
    Upstream Candidate: No. LGI specific NTP requirement: MVXREQ-886
    
    Signed-off-by: Oussema ben abdallah(obenabdallah@libertyglobal.com)

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -816,9 +816,10 @@ int do_toggle_v6_status (void)
         CcspTraceInfo(("%s %d toggle initiated\n", __FUNCTION__, __LINE__));
 
         ret = v_secure_system("echo 2 > /proc/sys/net/ipv6/conf/%s/accept_ra ; "
+                              "echo 0 > /proc/sys/net/ipv6/conf/%s/autoconf ; "
                               "sysctl -w net.ipv6.conf.%s.disable_ipv6=1 ; "
                               "sysctl -w net.ipv6.conf.%s.disable_ipv6=0",
-                              wanInterface, wanInterface, wanInterface);
+                              wanInterface, wanInterface, wanInterface, wanInterface);
 
         if (ret != 0) {
             CcspTraceWarning(("%s: Failure in executing command via v_secure_system. ret:[%d]\n", __FUNCTION__, ret));
