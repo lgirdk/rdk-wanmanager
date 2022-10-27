81117ab9452e1651e9024c112679c1e4e53b3386
Saikiran <svanamala@libertyglobal.com>
2022-10-27 17:27:17 +0200

    dual wan: add support of Upstream functionality ( OFW-3599 ) ( ** PARTIAL MERGE ** )
    RDK-wanmanager integration for docsis platforms, add upstream
    functionality.
    
    Upstream Candidate : May be, Part of dual wan support.
    
    Signed-off-by: Saikiran <svanamala@libertyglobal.com>

--- source/TR-181/middle_layer_src/wanmgr_rdkbus_utils.h
+++ source/TR-181/middle_layer_src/wanmgr_rdkbus_utils.h
@@ -127,6 +127,7 @@ extern token_t sysevent_token;
 #define CMAGENT_COMPONENT_NAME "eRT.com.cisco.spvtg.ccsp.cm"
 #define CMAGENT_COMP_NAME_WITHOUTSUBSYSTEM "com.cisco.spvtg.ccsp.cm"
 #define CMAGENT_COMPONENT_PATH "/com/cisco/spvtg/ccsp/cm"
+#define CMAGENT_UPSTREAM_NAME ".Upstream"
 
 //Cellular Manager
 #define CELLULAR_COMPONENT_NAME "eRT.com.cisco.spvtg.ccsp.cellularmanager"
