470963b6d4208b71e8934b64d605decd14cb6766
Devin Mungan <dmungan@libertyglobal.com>
2023-03-02 11:49:36 +0100

    fix missing close() after creating /tmp/phylink_wan_state_up ( OFW-4266 )
    Fix WAN interface fails to lease an IP.
    
    [Issue]
    After a while, udhcpc ran by wanmanager fails to lease IP for the WAN interface.
    This is because service_udhcpc process tries to communicate with wanmanager before
    leasing an IP but this communication fails due to the failure of accept() syscall
    because of a wanmanager bug that depletes the fd pool of the process.
    
    [Solution]
    Every wan-status:started event trigger a creat() syscall to create the file
    '/tmp/phylink_wan_state_up' but the caller does not close the associated fd.
    Returned fd should be closed.
    
    [Testing Performed]
    Find the pid of wanmanager (X)
    trigger sysevent set wan-status stopped && sysevent set wan-status started
    a couple of times
    Confirm /proc/X/fd list does not have any fd's associated to '/tmp/phylink_wan_state_up'
    
    Note: latest upstream has an alternative fix.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -17,6 +17,8 @@
  * limitations under the License.
 */
 
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sysevent/sysevent.h>
 #include <pthread.h>
 #include <syscfg/syscfg.h>
@@ -629,7 +631,6 @@ static void *WanManagerSyseventHandler(void *args)
         char cmd_str[BUF_SIZE] = {0};
         int namelen = sizeof(name);
         int vallen  = sizeof(val);
-	int fd = 0;
         async_id_t getnotification_asyncid;
         int err = 0;
         ANSC_STATUS result = 0;
@@ -724,15 +725,24 @@ static void *WanManagerSyseventHandler(void *args)
             }
             else if ((strcmp(name, SYSEVENT_WAN_STATUS) == 0) && (strcmp(val, SYSEVENT_VALUE_STARTED) == 0))
             {
+                int fd;
+
                 if (!lan_wan_started)
                 {
                     check_lan_wan_ready();
                 }
-                fd = creat("/tmp/phylink_wan_state_up",S_IRUSR| S_IWUSR| S_IRGRP| S_IROTH);
-		if(fd != -1)
-		{
-		    close(fd);
-		}
+
+                fd = open("/tmp/phylink_wan_state_up", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
+                if (fd == -1)
+                {
+                    CcspTraceError(("%s %d: Failed to create file: /tmp/phylink_wan_state_up, error = [%d][%s]\n", __FUNCTION__, __LINE__, errno, strerror(errno)));
+                }
+                else
+                {
+                    close(fd);
+                }
+
 #if defined(_DT_WAN_Manager_Enable_)
                                 sysevent_set(sysevent_fd, sysevent_token, "sendImmediateRA", "true", 0);
             }
