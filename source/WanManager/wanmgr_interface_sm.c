7c82c1bc628a9c4b6b5fb27b4020964969ddeca2
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2023-11-14 17:13:57 +0100

    reorder startup sequence of dhcpv4 and dhcpv6 clients ( OFW-5301 F3896SI-1846 )
    [BUG DESCRIPTION]
    ACS connection is sometimes first established over ipv4 url in dual stack mode. after ipv6 becomes available, ACS connection switches over to ipv6 url. This causes to send "4 VALUE CHANGE" after CPE has been rebooted.
    
    [FIX DESCRIPTION]
    This was handled in commit :- https://bitbucket.upc.biz/projects/RMC/repos/rdk-wanmanager/commits/99b82d2ac3d1cbe4e7441984b804d580449909c7, but these changes are no longer available after RDK rebase in rnl17
    
    Modified the code to start the dhcpv6 client first in case of dual stack mode.
    
    [TESTING PERFORMED]
    step(1) Perform factory reset on DUT in dual stack mode.
    step(2) start a packet capture between DUT and ACS Server
    step(3) Verify DUT establishes initial connection with ACS over ipv6 in dual stack mode.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -2153,6 +2153,16 @@ static eWanState_t wan_transition_wan_validated(WanMgr_IfaceSM_Controller_t* pWa
     }
     else
     {
+        if(p_VirtIf->IP.IPv6Source == DML_WAN_IP_SOURCE_DHCP && (p_VirtIf->IP.Dhcp6cPid == 0) &&
+            (p_VirtIf->IP.Mode == DML_WAN_IP_MODE_DUAL_STACK || p_VirtIf->IP.Mode == DML_WAN_IP_MODE_IPV6_ONLY))
+        {
+            /* Start all interface with accept ra disbaled */
+            WanMgr_Configure_accept_ra(p_VirtIf, FALSE);
+            /* Start DHCPv6 Client */
+            p_VirtIf->IP.Dhcp6cPid = WanManager_StartDhcpv6Client(p_VirtIf, pInterface->IfaceType);
+            CcspTraceInfo(("%s %d - Started dhcpv6 client on interface %s, dhcpv6_pid %d \n", __FUNCTION__, __LINE__, p_VirtIf->Name, p_VirtIf->IP.Dhcp6cPid));
+        }
+
         if (p_VirtIf->IP.IPv4Source == DML_WAN_IP_SOURCE_DHCP &&
             (p_VirtIf->IP.Mode == DML_WAN_IP_MODE_DUAL_STACK || p_VirtIf->IP.Mode == DML_WAN_IP_MODE_IPV4_ONLY))
         {
@@ -2166,15 +2176,6 @@ static eWanState_t wan_transition_wan_validated(WanMgr_IfaceSM_Controller_t* pWa
             CcspTraceInfo(("%s %d - Started dhcpc on interface %s, dhcpv4_pid %d \n", __FUNCTION__, __LINE__, p_VirtIf->Name, p_VirtIf->IP.Dhcp4cPid));
         }
 
-        if(p_VirtIf->IP.IPv6Source == DML_WAN_IP_SOURCE_DHCP && (p_VirtIf->IP.Dhcp6cPid == 0) &&
-            (p_VirtIf->IP.Mode == DML_WAN_IP_MODE_DUAL_STACK || p_VirtIf->IP.Mode == DML_WAN_IP_MODE_IPV6_ONLY))
-        {
-            /* Start all interface with accept ra disbaled */
-            WanMgr_Configure_accept_ra(p_VirtIf, FALSE);
-            /* Start DHCPv6 Client */
-            p_VirtIf->IP.Dhcp6cPid = WanManager_StartDhcpv6Client(p_VirtIf, pInterface->IfaceType);
-            CcspTraceInfo(("%s %d - Started dhcpv6 client on interface %s, dhcpv6_pid %d \n", __FUNCTION__, __LINE__, p_VirtIf->Name, p_VirtIf->IP.Dhcp6cPid));
-        }
     }
 
     if(strstr(pInterface->BaseInterface, "Cellular") != NULL)
