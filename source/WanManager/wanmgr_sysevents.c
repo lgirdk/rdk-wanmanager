6779dca85cb6c6751d844bc5b7cef1e12e06d0f7
Govindaraja <gnb.ext@libertyglobal.com>
2023-11-15 23:15:24 +0530

    add network log entry for wan interface status ( OFW-5239 )
    [Description]
    Network log entry for WAN Interface
    
    [Solution]
    Added a neccessary code changes to log the wan interface status events using syslog.
    
    [Testing]
    1. Make sure modem is UP and running.
    2. Verified the network logs data models.
    3. Verified the wan interface status events in network logs UI page.
    
    [Upstream Candidate]
    No
    
    Signed-off-by: Govindaraja N B <gnb.ext@libertyglobal.com>

--- source/WanManager/wanmgr_sysevents.c
+++ source/WanManager/wanmgr_sysevents.c
@@ -28,6 +28,8 @@
 #include "wanmgr_ipc.h"
 #include "wanmgr_utils.h"
 #include "secure_wrapper.h"
+#include <ccsp_syslog.h>
+#include "platform_hal.h"
 
 int sysevent_fd = -1;
 token_t sysevent_token;
@@ -763,6 +765,16 @@ static void *WanManagerSyseventHandler(void *args)
                 {
                     check_lan_wan_ready();
                 }
+#if defined(FEATURE_NETWORK_LOGS) && defined(_LG_MV3_)
+                char modelName[16];
+                if (platform_hal_GetModelName(modelName) == RETURN_OK)
+                {
+                    if (strcmp(modelName, "F5685LGE") == 0)
+                    {
+                        syslog_networklog("NETWORK",LOG_NOTICE,"%s","WAN Interface Up");
+                    }
+                }
+#endif
 
                 fd = open("/tmp/phylink_wan_state_up", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
@@ -782,12 +794,23 @@ static void *WanManagerSyseventHandler(void *args)
             {
                 needDibblerRestart = TRUE;
                 v_secure_system("sysevent set dibbler-restart false");
-
+#endif
             }
             else if ((strcmp(name, SYSEVENT_WAN_STATUS) == 0) && (strcmp(val, SYSEVENT_VALUE_STOPPED) == 0))
             {
+#if defined(_DT_WAN_Manager_Enable_)
                 needDibblerRestart = TRUE;
 	        sysevent_set(sysevent_fd, sysevent_token, "sendImmediateRA", "true", 0);
+#endif
+#if defined(FEATURE_NETWORK_LOGS) && defined(_LG_MV3_)
+                char modelName[16];
+                if (platform_hal_GetModelName(modelName) == RETURN_OK)
+                {
+                    if (strcmp(modelName, "F5685LGE") == 0)
+                    {
+                        syslog_networklog("NETWORK",LOG_ERR,"%s","WAN Interface down");
+                    }
+                }
 #endif
             }
 #if defined (RDKB_EXTENDER_ENABLED)
