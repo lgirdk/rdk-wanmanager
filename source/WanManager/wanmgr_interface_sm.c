ba6153138f0b9775acc7bbfef16a868ac4fa0725
Devin Mungan <dmungan@libertyglobal.com>
2024-07-18 19:08:27 +0300

    fix IPV6 Internet is not working for dslite mode ( OFW-5934 )
    [Issue]
    ra-lifetime is 0 therefore LAN client won't have default IPv6 route.
    
    [Description]
    ra-lifetime in zebra.conf is set as 0 if wan-status is not "started"
    when opensync is enabled, this is not a problem in practice because
    opensync triggers radv-restart after wan-status is set to "started"
    but when opensync is disabled, we rely on rdk-wanmanager's radv-restart
    which is triggered before wan-status is set to started.
    
    [Solution]
    move radvd-restart along with dhcps-restart and ipv6_connection_status
    setting after wan-status is set to "started"
    
    [Testing Performed]
    1. set eroutermode to DSLite
    2. disable opensync
    3. reboot device and confirm ra-lifetime is nonzero (/var/zebra.conf)
    
    Signed-off-by: Devin Mungan(dmungan.ext@libertyglobal.com)

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -1648,19 +1648,6 @@ static int wan_setUpIPv6(WanMgr_IfaceSM_Controller_t * pWanIfaceCtrl)
     }
 #endif
 
-    if (!strcmp(p_VirtIf->Alias, "DATA"))
-    {
-        uint32_t mtu = 0;
-        update_wan_mtu(&mtu);
-        if( mtu > 0)
-        {
-            snprintf(cmdStr, sizeof(cmdStr), "ip -6 link set %s mtu %u", p_VirtIf->IP.Ipv6Data.ifname,mtu);
-            WanManager_DoSystemAction("setupIPv6:", cmdStr);
-        }
-        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_IPV6_CONNECTION_STATE, WAN_STATUS_UP, 0);
-        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_RADVD_RESTART, NULL, 0);
-        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_DHCP_SERVER_RESTART, NULL, 0);
-    }
     sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_FIREWALL_RESTART, NULL, 0);
     sysevent_get(sysevent_fd, sysevent_token, SYSEVENT_WAN_STATUS, buf, sizeof(buf));
     //TODO: Firewall IPv6 FORWARD rules are not working if SYSEVENT_WAN_SERVICE_STATUS is set for REMOTE_IFACE. Modify firewall similar for backup interface similar to primary.
@@ -1711,7 +1698,21 @@ static int wan_setUpIPv6(WanMgr_IfaceSM_Controller_t * pWanIfaceCtrl)
         }
 #endif
     }
-  
+
+    if (!strcmp(p_VirtIf->Alias, "DATA"))
+    {
+        uint32_t mtu = 0;
+        update_wan_mtu(&mtu);
+        if( mtu > 0)
+        {
+            snprintf(cmdStr, sizeof(cmdStr), "ip -6 link set %s mtu %u", p_VirtIf->IP.Ipv6Data.ifname,mtu);
+            WanManager_DoSystemAction("setupIPv6:", cmdStr);
+        }
+        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_IPV6_CONNECTION_STATE, WAN_STATUS_UP, 0);
+        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_RADVD_RESTART, NULL, 0);
+        sysevent_set(sysevent_fd, sysevent_token, SYSEVENT_DHCP_SERVER_RESTART, NULL, 0);
+    }
+
     return ret;
 }
 
