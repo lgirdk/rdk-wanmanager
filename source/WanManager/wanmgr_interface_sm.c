9a37b0a3750adc0f52f967160b1f9ea4669b3038
Durga Prasad D <ddemali.ext@libertyglobal.com>
2024-08-22 15:34:48 +0530

    implement new event markers for IPv6 loss on mg0 and voip0 ( OFW-6028 )
    [Issue]
    Introducing new event markers for IPv6 loss on mg0 and voip0.
    
    [Decription]
    Current stack supports erouter IPv6 loss marker only.Introduced new markers for mg0 and voip0.
    
    [Solution]
    Implemented event markers for IPv6 loss on mg0 and voip0.
    
    [Testing]
    Verified the markers in ODH Server.
    
    [Upstream Candidate]
    Maybe
    
    Signed-off-by: Durga Prasad D <ddemali.ext@libertyglobal.com>

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -460,6 +460,7 @@ static void WanMgr_MonitorDhcpApps (WanMgr_IfaceSM_Controller_t* pWanIfaceCtrl)
                 char guAddr[IP_ADDR_LENGTH] = {0};
                 uint32_t prefixLen = 0;
                 bool IPv6AddrMonEnabled = FALSE;
+                char rferroripv6loss [64];
 
                 WanMgr_Config_Data_t*   pWanConfigData = WanMgr_GetConfigData_locked();
                 if(pWanConfigData != NULL)
@@ -482,7 +483,8 @@ static void WanMgr_MonitorDhcpApps (WanMgr_IfaceSM_Controller_t* pWanIfaceCtrl)
                     /* stop the client so that WanMgr_MonitorDhcpApps function will start it when the client gracefully stops */
                     WanManager_StopDhcpv6Client(p_VirtIf->Name, STOP_DHCP_WITH_RELEASE);
 #ifdef ENABLE_FEATURE_TELEMETRY2_0
-                    t2_event_d("RF_ERROR_erouter_ipv6_loss", 1);
+                    snprintf(rferroripv6loss, sizeof(rferroripv6loss), "RF_ERROR_%s_ipv6_loss", p_VirtIf->Name);
+                    t2_event_d(rferroripv6loss, 1);
 #endif
                 }
                 p_VirtIf->IP.Ipv6AddrMonTrigger = FALSE;
